786b718aef2a516e295517340455e41917c8909e
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-03-14 17:39:41 +0100

    fim de la app pokemon

--- src/modulorum/pokemon/components/PokemonOptiones.vue
+++ src/modulorum/pokemon/components/PokemonOptiones.vue
@@ -5,10 +5,15 @@
   <section class=" flex flex-col mt-8">
 
     <button 
+    
+    :disabled="nonEligere"
     @click="$emit('electusOptio', id)"
     v-for="{ nomen, id} in optiones" 
     :key="id" 
-    class="capitalize"
+    :class="['capitalize disabled:shadow-none disabled:bg-gray-100', {
+      'recte': id === recteResponsio && nonEligere,
+      'non-recte': id !== recteResponsio && nonEligere
+    }]"
     >
 
 {{ nomen }}
@@ -22,12 +27,14 @@
 </template>
 
 <script setup lang="ts">
-import type { Pokemon } from '../interfaces';
+import { LudumStatus, type Pokemon } from '../interfaces';
 
 
 
 interface Props {
     optiones: Pokemon[];
+    nonEligere: boolean;
+    recteResponsio: number;
 }
 defineProps<Props>();
 
@@ -40,5 +47,13 @@ defineEmits<{
 @import 'tailwindcss';
 button {
     @apply bg-yellow-400 shadow-lg rounded-full  p-3 my-2 w-40 text-center  cursor-pointer  hover:bg-yellow-300;
+}
+
+.recte{
+    @apply bg-blue-500 text-black;
+}
+
+.non-recte{
+    @apply bg-gray-100 text-black;
 }
 </style>
